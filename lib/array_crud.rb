4149f27e1801d90b8e6249c83e21446172fc26d7
weng lin <wenglin@buffalo.edu>
2019-12-17 03:48:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.push("Apple")
+  add_element_to_end_of_array.push()
+  p add_element_to_end_of_array
 end
 
 def add_element_to_start_of_array(array, element)
